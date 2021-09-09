b05412925d1b5545fd2df2fb4be3d3e3392a5741
Eddy Martinez <eddymartinezsaldana@gmail.com>
2021-09-09 20:37:57 +0100

    Adds challenges/

---
+++ api_host.py
@@ -0,0 +1,10 @@
+"""
+- Create a simple database containing entries for some made up users and insert some data
+    - Create an API that connects to that database and allows users to perform CRUD operations
+    - C: Implement GET methods to get different entries from different tables using query string parameters
+    - R: Implement POST methods to create a new entry
+    - U: Implement PUT methods to update an existing entry
+    - D: Implement DELETE methods to delete an existing entry by id
+    - Be careful what you do with this code if it contains your database credentials
+    """
+
